76dcfca5c70977d5a35411d49b48c88319385527
PushkarRJain <prj20012016@gmail.com>
2021-09-22 10:19:17 +0530

    Updates

---
+++ precache-manifest.f9d388ece1775ca5a41fee9e07a14f98.js
@@ -0,0 +1,262 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7b4ffd21653f09a14f07c0dcb29e537",
+    "url": "/portfoliowebsite/index.html"
+  },
+  {
+    "revision": "1e13b05cb843892bcb19",
+    "url": "/portfoliowebsite/static/css/main.41f14986.chunk.css"
+  },
+  {
+    "revision": "a42a3a02a415a3130ed7",
+    "url": "/portfoliowebsite/static/js/2.75877e0c.chunk.js"
+  },
+  {
+    "revision": "1e13b05cb843892bcb19",
+    "url": "/portfoliowebsite/static/js/main.5b5d4210.chunk.js"
+  },
+  {
+    "revision": "04c167eac895b4b58985",
+    "url": "/portfoliowebsite/static/js/runtime-main.9f46e7ed.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfoliowebsite/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/portfoliowebsite/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/portfoliowebsite/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/portfoliowebsite/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/portfoliowebsite/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/portfoliowebsite/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/portfoliowebsite/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfoliowebsite/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "3b021f8ffcf9f323ea72abc28382e234",
+    "url": "/portfoliowebsite/static/media/address_image.3b021f8f.svg"
+  },
+  {
+    "revision": "6c8410d0dc11fec239040695be13315d",
+    "url": "/portfoliowebsite/static/media/animated_ashutosh.6c8410d0.png"
+  },
+  {
+    "revision": "a633be2faf9b129259563d65d0aa1333",
+    "url": "/portfoliowebsite/static/media/animated_logo.a633be2f.svg"
+  },
+  {
+    "revision": "b6b12c7d07e0569e771c8aedf81a7767",
+    "url": "/portfoliowebsite/static/media/blogs_image.b6b12c7d.svg"
+  },
+  {
+    "revision": "dfcb7a585547c0b125d208b980d6f57f",
+    "url": "/portfoliowebsite/static/media/cloud_infrastructure.dfcb7a58.svg"
+  },
+  {
+    "revision": "b3685be8bafcd5fe4786b39cba69a561",
+    "url": "/portfoliowebsite/static/media/codeInLogo.b3685be8.png"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/portfoliowebsite/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "434d2b012c3efc3da1ef90549a50f46b",
+    "url": "/portfoliowebsite/static/media/data_science.434d2b01.svg"
+  },
+  {
+    "revision": "7a9b2eb087350abacd2587dd578226c7",
+    "url": "/portfoliowebsite/static/media/deeplearning_ai_logo.7a9b2eb0.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfoliowebsite/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "f65ee6f64b46db0ae0f59279ea7853a8",
+    "url": "/portfoliowebsite/static/media/dsc_logo.f65ee6f6.png"
+  },
+  {
+    "revision": "caed892b2a0b817a8e275ae29638edaf",
+    "url": "/portfoliowebsite/static/media/education.caed892b.svg"
+  },
+  {
+    "revision": "85d6957f6a0d678d1ac3932e5be4fcb2",
+    "url": "/portfoliowebsite/static/media/experience.85d6957f.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/portfoliowebsite/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/portfoliowebsite/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "9d204f50ec8e5bcc5dcda0132d404b48",
+    "url": "/portfoliowebsite/static/media/fa-brands-400.9d204f50.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/portfoliowebsite/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/portfoliowebsite/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/portfoliowebsite/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "9443c1e7fdf95d739af3f0901224f90a",
+    "url": "/portfoliowebsite/static/media/fa-regular-400.9443c1e7.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/portfoliowebsite/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/portfoliowebsite/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/portfoliowebsite/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/portfoliowebsite/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/portfoliowebsite/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "3d102342391af184d5ae9e7708d8220f",
+    "url": "/portfoliowebsite/static/media/fa-solid-900.3d102342.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/portfoliowebsite/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/portfoliowebsite/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "a73310ea0ad8aebedb65b3106024427a",
+    "url": "/portfoliowebsite/static/media/feelingProud.a73310ea.svg"
+  },
+  {
+    "revision": "589bec7d632ecff9672dc4da97897bbd",
+    "url": "/portfoliowebsite/static/media/fullstack.589bec7d.svg"
+  },
+  {
+    "revision": "ba18401b15f40a463898b706cc6ff08c",
+    "url": "/portfoliowebsite/static/media/gcp_logo.ba18401b.png"
+  },
+  {
+    "revision": "e64de169a4222787bc49f3706282ef96",
+    "url": "/portfoliowebsite/static/media/github_logo.e64de169.png"
+  },
+  {
+    "revision": "0c8a4c114ad00f7854e91550379b9b91",
+    "url": "/portfoliowebsite/static/media/googleAssistant.0c8a4c11.jpg"
+  },
+  {
+    "revision": "c24ba79111a90758a3a472a906ab2901",
+    "url": "/portfoliowebsite/static/media/googleAssistant.c24ba791.svg"
+  },
+  {
+    "revision": "6d325fec19fe3b6b169d9012f9a1dfb9",
+    "url": "/portfoliowebsite/static/media/ibm_logo.6d325fec.png"
+  },
+  {
+    "revision": "29152cf756a1facead5393c32e373371",
+    "url": "/portfoliowebsite/static/media/intel_logo.29152cf7.jpg"
+  },
+  {
+    "revision": "b05ae5b575a7e1125610944dc152c35f",
+    "url": "/portfoliowebsite/static/media/jsFramework.b05ae5b5.svg"
+  },
+  {
+    "revision": "4ba886bafb9ff0a5b71b800c47f45d90",
+    "url": "/portfoliowebsite/static/media/legato_logo.4ba886ba.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfoliowebsite/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "db6c1c0dddd4c868e00a3cffa1078e49",
+    "url": "/portfoliowebsite/static/media/muffito_logo.db6c1c0d.png"
+  },
+  {
+    "revision": "cd7077f7a08bffdb11e13093424e13fd",
+    "url": "/portfoliowebsite/static/media/nptel_logo.cd7077f7.png"
+  },
+  {
+    "revision": "14b4bfd9960873d66a2f9ac511c71492",
+    "url": "/portfoliowebsite/static/media/optaiot_logo.14b4bfd9.png"
+  },
+  {
+    "revision": "b0c55b3cefa8817926c2e4defe265bea",
+    "url": "/portfoliowebsite/static/media/pict_logo.b0c55b3c.png"
+  },
+  {
+    "revision": "2372fd35a141275e86588fe93d5b26c8",
+    "url": "/portfoliowebsite/static/media/pj.2372fd35.png"
+  },
+  {
+    "revision": "6e7dc5ad476dd2d57600bca9521f3a5d",
+    "url": "/portfoliowebsite/static/media/portfolio.6e7dc5ad.gif"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/portfoliowebsite/static/media/programmer.c1a95f73.svg"
+  },
+  {
+    "revision": "f7964c6bf45e77547f1e5aae1e1dede0",
+    "url": "/portfoliowebsite/static/media/projects_image.f7964c6b.svg"
+  },
+  {
+    "revision": "bd5f4148a754968cb4444206539c565c",
+    "url": "/portfoliowebsite/static/media/pwa.bd5f4148.png"
+  },
+  {
+    "revision": "da0e7c80b90df28dd3a590c80f3d4970",
+    "url": "/portfoliowebsite/static/media/stanford_logo.da0e7c80.png"
+  },
+  {
+    "revision": "de3da8064f2e9ffab945772c2c67e46f",
+    "url": "/portfoliowebsite/static/media/talksCardBack.de3da806.svg"
+  },
+  {
+    "revision": "7f0feeedda08cf7d68593454bc2e82b5",
+    "url": "/portfoliowebsite/static/media/talksCardBackAlt.7f0feeed.svg"
+  },
+  {
+    "revision": "67a6a11c47edafb87cf24da66afe3dca",
+    "url": "/portfoliowebsite/static/media/ui_ux_design.67a6a11c.svg"
+  }
+]);
